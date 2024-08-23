8f980333868001aa438ae29b4979588fae5d15f8
awcl <113561942+awcl@users.noreply.github.com>
2024-08-23 16:26:07 -0600

    Update buildrun.bat

--- buildrun.bat
+++ buildrun.bat
@@ -3,13 +3,13 @@ setlocal
 
 set "EXE_NAME=run.exe"
 
-if exist %EXE_NAME% (
-    del %EXE_NAME%
+if exist "%EXE_NAME%" (
+    del "%EXE_NAME%"
 )
 
 echo Building...
 
-go build -o %EXE_NAME% slideshow.go
+go build -o "%EXE_NAME%" slideshow.go
 
 if errorlevel 1 (
     echo Build failed. Exiting...
@@ -20,13 +20,24 @@ echo Build succeeded
 
 echo Running...
 
-start "" /b %EXE_NAME%
+start "" /b "%EXE_NAME%"
 
-for /f "tokens=2 delims=:" %%I in ('"ipconfig | findstr /i "IPv4""') do set IP_ADDRESS=%%I
+setlocal enabledelayedexpansion
+set "IP_ADDRESSES="
 
-set IP_ADDRESS=%IP_ADDRESS:~1%
+for /f "tokens=2 delims=:" %%I in ('ipconfig ^| findstr /i "IPv4"') do (
+    set "IP_ADDRESS=%%I"
+    set "IP_ADDRESS=!IP_ADDRESS:~1!"
+    if defined IP_ADDRESS (
+        if not "!IP_ADDRESSES!"=="" set "IP_ADDRESSES=!IP_ADDRESSES!, "
+        set "IP_ADDRESSES=!IP_ADDRESSES!!IP_ADDRESS!"
+    )
+)
 
 echo.
-echo Users on the LAN can access it at http://%IP_ADDRESS%:3000/
+echo Users on the LAN(s) can access it at:
+for %%A in (%IP_ADDRESSES%) do echo http://%%A:3000/
 echo.
 echo If you want to stop the application, please close this command window
+
+pause
